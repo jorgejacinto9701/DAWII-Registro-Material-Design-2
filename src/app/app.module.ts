83ad3cc2e9f66856de23e7e238fdbf7c97ca6f6c
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-02-20 10:09:17 -0500

    modulo de material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
 import { AppComponent } from './app.component';
 import { AddModalidadComponent } from './components/add-modalidad/add-modalidad.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatButtonModule } from '@angular/material/button';
-import { MatNativeDateModule} from '@angular/material/core';
 import { AppMaterialModule } from './app.material.module';
 
 
