6bbfa1077e1c3de0da20e30d89d1628ddeb5399b
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-02-19 21:35:09 -0500

    Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { AddModalidadComponent } from './components/add-modalidad/add-modalidad.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule} from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddModalidadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatNativeDateModule ,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/add-modalidad/add-modalidad.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Deporte } from 'src/app/models/deporte.model';
+import { Modalidad } from 'src/app/models/modalidad.model';
+import { DeporteService } from 'src/app/services/deporte.service';
+import { ModalidadService } from 'src/app/services/modalidad.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
+import Swal from 'sweetalert2'
+
+@Component({
+  selector: 'app-add-modalidad',
+  templateUrl: './add-modalidad.component.html',
+  styleUrls: ['./add-modalidad.component.css']
+})
+export class AddModalidadComponent implements OnInit {
+
+  formsRegistra= this.formBuilder.group({
+    nombre:  ['', [Validators.required, Validators.pattern('[a-zA-ZáéíóúÁÉÍÓÚñ0-9 ]{3,30}')]],
+    numHombres: ['', [Validators.required, Validators.pattern('[0-9]{1,2}')]],
+    numMujeres: ['', [Validators.required, Validators.pattern('[0-9]{1,2}')]],
+    edadMinima: ['', [Validators.required, Validators.pattern('[0-9]{2}'), Validators.min(18), Validators.max(25)]],
+    edadMaxima: ['', [Validators.required, Validators.pattern('[0-9]{2}'), Validators.min(18), Validators.max(40), Validators.max(40)]],
+    sede: ['', [Validators.required, Validators.pattern('[a-zA-ZáéíóúÁÉÍÓÚñ0-9 ]{3,30}')]],
+    deporte: ['', [Validators.min(0)]],
+});
+
+
+
+
+  lstDeportes: Deporte [] = [] ;
+  objModalidad: Modalidad = {
+    deporte:{
+      idDeporte:-1,
+    }
+  };
+
+  constructor(private formBuilder: FormBuilder, private deporteService:DeporteService, private modalidadService: ModalidadService) { 
+      this.deporteService.listaDeporte().subscribe(
+          x => this.lstDeportes = x
+      );
+  }
+
+  registra(){
+    if (this.formsRegistra.valid) {
+        this.modalidadService.registraModalidad(this.objModalidad).subscribe(
+          x => Swal.fire({icon: 'info',title: 'Resultado del Registro',text: x.errores}) 
+        );
+    }
+  }  
+
+  ngOnInit(): void {
+  }
+
+  
+
+}
